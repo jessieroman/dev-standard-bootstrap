07f9baf3bf6fdf3fa7def9d9fc4df38c9d7105e1
Jessie Roman <roman.a.jessie@gmail.com>
2026-09-22 19:17:37 -0400

    chore: bump DEV_STANDARDS_REF pin to dev-standard v0.10.2 (25693d6)
    Refs: dev-standards-cd3

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ set -eu
 
 repo_url="${DEV_STANDARDS_REPO_URL:-git@github.com:jessieroman/dev-standard.git}"
 dest="${DEV_STANDARDS_DIR:-$HOME/git/dev-standards}"
-ref="${DEV_STANDARDS_REF:-0097041802286b4034e686373d3cdbb0949f6526}"
+ref="${DEV_STANDARDS_REF:-25693d6ca805ebb1a68194953dfc4dcb8be61c4d}"
 
 log() { printf '\033[1;32m==>\033[0m %s\n' "$1"; }
 die() { printf '\033[1;33m!!\033[0m %s\n' "$1" >&2; exit 1; }
